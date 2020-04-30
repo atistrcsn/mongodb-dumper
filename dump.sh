f9edd2b2ecef803e04231dfa24fbc3ec74f54ee9
Attila Turcsan <atistrcsn@users.noreply.github.com>
2020-04-30 11:30:11 +0200

    update dump.sh to exec in running container

--- dump.sh
+++ dump.sh
@@ -1,8 +1,7 @@
 help () {
-    echo 'There are 4 available options:'
+    echo 'There are 3 available options:'
     echo
-    echo '-h (required) -- host name'
-    echo '-p            -- port'
+    echo '-c (required) -- running container name'
     echo '-d            -- database name; if not set - all database will be dumped'
     echo '-o            -- additional options for mongodump command; wrap in double quotas all the options'
 }
@@ -13,16 +12,15 @@ if [ $1 == '--help' ]
     exit
 fi
 
-host_check=0
+container_check=0
 
-while getopts :h:p:f:d:o: option
+while getopts :c:d:o: option
   do
     case "${option}"
         in
-        h) HOST=${OPTARG}; host_check=1;;
+        c) CONTAINER=${OPTARG}; container_check=1;;
         d) DBNAME=${OPTARG};;
         o) OPTIONS=${OPTARG};;
-        p) PORT=${OPTARG};;
         :)
             echo "Option $option has to be set"
             help
@@ -32,20 +30,18 @@ while getopts :h:p:f:d:o: option
 done
 
 
-if [[ $host_check == 0 ]]
+if [[ $container_check == 0 ]]
 then
-    echo "-f and -h options must be set"
+    echo "-c option must be set"
     help
     exit
 fi
 
-HOST=${HOST:-"localhost"}
-PORT=${PORT:-27017}
 OPTIONS=${OPTIONS:-""}
 if [ -n DBNAME ]
  then DB="--db=$DBNAME"
  else DB=""
 fi
 
-docker run --rm mongo /bin/bash -c "mongodump --host $HOST --port $PORT --archive --gzip $DB $OPTIONS" | \
+docker exec ${CONTAINER} /bin/bash -c "mongodump --host localhost --port 27017 --archive --gzip $DB $OPTIONS" | \
    cat > dump_${DBNAME}_$(date '+%d-%m-%Y_%H-%M-%S').archive
